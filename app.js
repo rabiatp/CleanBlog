192da6d083fdc1f30dbb0ef951c9d1cd2325b959
rabiatp <{username}@gmail.com>
2022-08-08 17:52:20 +0300

    CleanBlog 1. Asama

---
+++ app.js
@@ -0,0 +1,11 @@
+const express = require('express')
+const app = express()
+
+app.get('/', (req, res) => {
+  res.send('Hello World')
+})
+
+const port = 3000
+app.listen(port, () => {
+    console.log(`Sunucu ${port} bağlantı kuruldu`)
+})
