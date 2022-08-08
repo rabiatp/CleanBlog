1fb46807fe8dba35a91ce7ad59eab7c1df7a9b3c
rabiatp <{username}@gmail.com>
2022-08-08 18:00:40 +0300

    CleanBlog update

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require('express')
 const app = express()
 
 app.get('/', (req, res) => {
-  res.send('Hello World')
+  const blog = { id: 1, title: "Blog title", description: "Blog description" }
+  res.send(blog)
 })
 
 const port = 3000
