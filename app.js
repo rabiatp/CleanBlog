b5e28eb054ff58b6864d06b2616caf6f8d1951fd
rabiatp <{username}@gmail.com>
2022-08-09 13:52:39 +0300

    2.BOLUM

--- app.js
+++ app.js
@@ -1,12 +1,28 @@
 const express = require('express')
 const app = express()
+const path = require('path')
+let ejs = require('ejs')
+
+
+app.use(express.static('public'))
+
+app.set('view engine', 'ejs');
 
 app.get('/', (req, res) => {
-  const blog = { id: 1, title: "Blog title", description: "Blog description" }
-  res.send(blog)
+  res.render('index')
+})
+app.get('/about.html', (req, res) => {
+  res.render('about')
+})
+app.get('/add_post.html', (req, res) => {
+  res.render('add_post')
+})
+app.get('/index.html', (req, res) => {
+  res.render('index')
 })
 
 const port = 3000
+
 app.listen(port, () => {
     console.log(`Sunucu ${port} bağlantı kuruldu`)
 })
